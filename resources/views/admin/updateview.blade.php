18b7a584c483ed5334efe47174462592f3ea40f4
katma restanto <katmarestanto@katmas-MacBook-Air.local>
2022-04-11 18:43:07 +0700

    minggu 8

--- resources/views/admin/updateview.blade.php
+++ resources/views/admin/updateview.blade.php
@@ -1,11 +1,12 @@
+<x-app-layout>
+
+</x-app-layout>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    
-
-  @include('admin.css')
-
-  <style type="text/css">
+    <bash href="/public">
+    @include('admin.css')
+    <style type="text/css">
     .title
     {
       color:white; padding-top: 25px; font-size: 25px;
@@ -14,21 +15,18 @@
       display:inline-block;
       width: 200px;
     }
-  </style>
-
+    </style>
   </head>
   <body>
     
-      <!-- partial -->
-      
-      @include('admin.sidebar')
+    <!-- partial -->
 
+    @include('admin.sidebar') 
+    @include('admin.navbar')
 
-      @include('admin.navbar')
-
-        <!-- partial -->
+    <!-- partial -->
         
-        <div class="container-fluid page-body-wrapper">
+    <div class="container-fluid page-body-wrapper">
         <div class="container" align="center">
         <h1 class="title">Add Product</h1>
 
@@ -43,36 +41,40 @@
         </div>
         @endif
 
-    
-
-        <form action="{{url('uploadproduct')}}" method="post" enctype="multipart/form-data">
+        <form action="{{url('updateproduct', $data->id)}}" method="post" enctype="multipart/form-data">
 
         @csrf
         <div style="padding:15px;">
           <label>Product Title</label>
-          <input style="color:black;" type="text" name="title" placeholder="Give a product title"
+          <input style="color:black;" type="text" name="title" value="{{$data->title}}"
           required="">
         </div>
 
         <div style="padding:15px;">
           <label>Price</label>
-          <input style="color:black;" type="number" name="price" placeholder="Give a price"
+          <input style="color:black;" type="number" name="price" value="{{$data->price}}"
           required="">
         </div>
 
         <div style="padding:15px;">
           <label>Description</label>
-          <input style="color:black;" type="text" name="des" placeholder="Give a description"
+          <input style="color:black;" type="text" name="des" value="{{$data->description}}"
           required="">
         </div>
 
         <div style="padding:15px;">
           <label>Quantity</label>
-          <input style="color:black;" type="text" name="quantity" placeholder="Product Quantity"
+          <input style="color:black;" type="text" name="quantity" value="{{$data->quantity}}"
           required="">
         </div>
 
         <div style="padding:15px;">
+          <label>Old Image</label>
+          <img height ="100" width="100" src="/productimage/{{$data->image}}">
+        </div>
+
+        <div style="padding:15px;">
+          <label>Change the image</label>
           <input type="file" name="file">
         </div>
 
@@ -84,9 +86,9 @@
         </div>
         </div>
 
-          <!-- partial -->
-        
-
-          @include('admin.script')
+    <!-- partial -->
+    
+    @include('admin.script')
+       
   </body>
 </html>
